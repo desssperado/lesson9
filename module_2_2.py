7baa106a76c7bb3d014844ecfa3923bda9c0a69d
desssperado <gamzatgasanov0@gmail.com>
2024-09-13 14:45:14 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+#Ввод трех целых чисел
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+#Проверка условий
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
